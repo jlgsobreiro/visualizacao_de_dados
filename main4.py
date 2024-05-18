d5683c239152c731aa47b09d928375f6bb28f7f7
jlgsobreiro <jlgsobreiro@gmail.com>
2024-05-17 22:02:43 -0300

    wip

--- main4.py
+++ main4.py
@@ -1,6 +1,25 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
+co_ = 0.25 * 10.0**(-3)
+g = 6*10**(4)
+k = 2 * 10**(3)
+kt = 0.9 * 10**(-9)
+n = 5 * 10**(2)
+
+ca0 = 0.2
+co0 = 0.0
+cp0 = 0.0
+
+ra = -k * ca0**n * co0**n
+ro = (1/2)*ra
+rp = -(1/2)*ra
+kla = kt * n**3 * g ** (1/2)
+
+vdcadt = lambda v: ra*v
+vdcodt = lambda v: kla * (co_ - co0)*v - ro*v
+vdcpdt = lambda v: rp*v
+
 ca0 = 0.1
 k = 1.3
 cb0 = 0
